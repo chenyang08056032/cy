99bc40b364e7b493bfd4ab988b610204bad9c99e
cy <chenyang08056032@163.com>
2026-01-13 18:17:42 +0800

    add

--- newHDK/run.sh
+++ newHDK/run.sh
@@ -1,10 +1,9 @@
-export PYTHONPATH=/workspace/sglang_test:$PYTHONPATH
 # 后台执行所有 Ascend 测试，日志保存到 /data/ascend_all_logs
-mkdir -p /data/ascend_all_logs
-LOG_FILE="/data/ascend_all_logs/console_$(date +%Y%m%d_%H%M%S).log"
+mkdir -p /data/c30044170/log
+LOG_FILE="/data/c30044170/log/console_$(date +%Y%m%d_%H%M%S).log"
 nohup python run_suite.py \
   --suite all \
   --continue-on-error \
   --enable-retry \
-  --log-dir /data/ascend_all_logs \
+  --log-dir /data/c30044170/log \
   > ${LOG_FILE} 2>&1 &
