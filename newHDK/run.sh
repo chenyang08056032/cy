600ef935024193375026f0727ecbf6db3acb5050
cy <chenyang08056032@163.com>
2026-01-13 18:42:43 +0800

    add

--- newHDK/run.sh
+++ newHDK/run.sh
@@ -1,9 +1,10 @@
 # 后台执行所有 Ascend 测试，日志保存到 /data/ascend_all_logs
 mkdir -p /data/c30044170/log
-LOG_FILE="/data/c30044170/log/console_$(date +%Y%m%d_%H%M%S).log"
+TIME=$(date +%Y%m%d_%H%M%S)
 nohup python run_suite.py \
-  --suite all \
+  --suite per-commit-1-npu-a3 \
   --continue-on-error \
   --enable-retry \
+  --timeout-pre-file 3600\
   --log-dir /data/c30044170/log \
-  > ${LOG_FILE} 2>&1 &
+ | split -b 10M -d -a 3 - /data/c30044170/log/console_${TIME}_part_2>&1 &
