8c2aa076be908196c64f5ad6624658feef8b4d00
cy <chenyang08056032@163.com>
2026-03-23 21:49:30 +0800

    add

--- 20260323/double_pd/run_qwen3_235b_p_1.sh
+++ 20260323/double_pd/run_qwen3_235b_p_1.sh
@@ -14,7 +14,7 @@ python3 -m sglang.launch_server \
     --device npu \
     --tp-size 16 \
     --attn-cp-size 2 \
-    --mem-fraction-static 0.66 \
+    --mem-fraction-static 0.67 \
     --max-running-requests 1 \
     --host 172.22.3.71 \
     --port 8000
