72ff5b03dec2072f29b4eb310154ade9bbfd959b
cy <chenyang08056032@163.com>
2026-03-24 21:24:05 +0800

    add

--- deepseek/dingshirenwu.sh
+++ deepseek/dingshirenwu.sh
@@ -4,6 +4,6 @@ for script in run_testcase_dsr1_w4a8_1p1d_16p_3k5_1k5_50ms.sh \
               run_testcase_dsr1_w8a8_2p1d_32p_3k5_1k5_20ms.sh
 do
     echo "开始执行: $script"
-    timeout 3h ./"$script" || echo "超时或失败: $script"
+    timeout 3h bash "$script" || echo "超时或失败: $script"
     echo "完成: $script"
 done
