e85615c9c0132e60baff144493a73569f53b7c5b
cy <chenyang08056032@163.com>
2026-03-25 16:22:46 +0800

    add

--- 20260323/pd_24p/run_qwen3_235b.sh
+++ 20260323/pd_24p/run_qwen3_235b.sh
@@ -68,7 +68,7 @@ do
       --max-running-requests 32 \
       --host ${D_IP[$i]} \
       --port 8232 \
-      --moe-a2a-backend ascend_fuseep
+      --moe-a2a-backend deepep --deepep-mode low_latency
       NODE_RANK=$i
       break
     fi
