4021881f3b3f470319b265226772ac3d42871481
cy <chenyang08056032@163.com>
2026-03-25 16:00:09 +0800

    add

--- 20260323/pd_24p/run_qwen3_235b.sh
+++ 20260323/pd_24p/run_qwen3_235b.sh
@@ -52,6 +52,7 @@ do
     then
       echo "${D_IP[$i]}"
       export SGLANG_DEEPEP_BF16_DISPATCH=1
+      export SGLANG_NPU_FUSED_MOE_MODE=1
       python3 -m sglang.launch_server \
       --model-path ${MODEL_PATH} \
       --disaggregation-mode decode \
