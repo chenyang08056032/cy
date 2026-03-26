1887783e4f58b3e92cca4e87ac6b323af12c0b78
cy <chenyang08056032@163.com>
2026-03-26 18:26:26 +0800

    add

---
+++ Long-term-stability-tese/Qwen3.5-397B-A17B-w8a8_decode.sh
@@ -0,0 +1,82 @@
+# high performance cpu
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+sysctl -w vm.swappiness=0
+sysctl -w kernel.numa_balancing=0
+sysctl -w kernel.sched_migration_cost_ns=50000
+# bind cpu
+export SGLANG_SET_CPU_AFFINITY=1
+
+unset https_proxy
+unset http_proxy
+unset HTTPS_PROXY
+unset HTTP_PROXY
+export ASCEND_LAUNCH_BLOCKING=1
+# cann
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+source /usr/local/Ascend/nnal/atb/set_env.sh
+
+export SGLANG_DISAGGREGATION_WAITING_TIMEOUT=3600
+export STREAMS_PER_DEVICE=32
+export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=64
+export HCCL_BUFFSIZE=2500
+export HCCL_OP_EXPANSION_MODE=AIV
+export HCCL_SOCKET_IFNAME=enp196s0f0
+export GLOO_SOCKET_IFNAME=enp196s0f0
+export SGLANG_NPU_PROFILING=0
+#export SGLANG_NPU_PROFILING_STAGE
+export DEEPEP_NORMAL_LONG_SEQ_ROUND=32
+export DEEPEP_NORMAL_LONG_SEQ_PER_ROUND_TOKENS=4096
+export ASCEND_MF_STORE_URL="tcp://172.22.3.71:24669"
+export SGLANG_DISAGGREGATION_BOOTSTRAP_TIMEOUT=3600
+export SGLANG_DISAGGREGATION_WAITING_TIMEOUT=3600
+export SGLANG_ENABLE_SPEC_V2=1
+export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=1
+
+# p节点IP
+P_IP=('172.22.3.71' '172.22.3.166')
+# D节点IP D节点首节点IP
+D_IP=('172.22.3.160')
+LOCAL_HOST1=`hostname -I|awk -F " " '{print$1}'`
+LOCAL_HOST2=`hostname -I|awk -F " " '{print$2}'`
+echo "${LOCAL_HOST1}"
+echo "${LOCAL_HOST2}"
+
+MODEL_PATH=/root/.cache/modelscope/hub/models/QwEco-Tech/Qwen3.5-397B-A17B-w8a8-mtp
+
+for i in "${!D_IP[@]}"; do
+  if [[ "$LOCAL_HOST1" == "${D_IP[$i]}" || "$LOCAL_HOST2" == "${D_IP[$i]}" ]]; then
+    python3 -m sglang.launch_server \
+      --model-path ${MODEL_PATH} \
+      --attention-backend ascend \
+      --device npu \
+      --tp-size 16 \
+      --nnodes 1 \
+      --node-rank 0 \
+      --chunked-prefill-size -1 \
+      --max-prefill-tokens 65536 \
+      --disable-radix-cache \
+      --trust-remote-code \
+      --host 0.0.0.0 \
+      --max-running-requests 256 \
+      --moe-a2a-backend deepep \
+      --deepep-mode low_latency \
+      --mem-fraction-static 0.85 \
+      --port 8000 \
+      --cuda-graph-bs 1 2 3 4 8 9 10 11 12 13 14 15 16 \
+      --quantization modelslim \
+      --enable-multimodal \
+      --mm-attention-backend ascend_attn \
+      --max-total-tokens 1200000 \
+      --dtype bfloat16 \
+      --mamba-ssm-dtype bfloat16 \
+      --disaggregation-mode decode \
+      --disaggregation-transfer-backend ascend \
+      --speculative-draft-model-quantization unquant \
+      --speculative-algorithm NEXTN \
+      --speculative-num-steps 3 \
+      --speculative-eagle-topk 1 \
+      --speculative-num-draft-tokens 4
+    NODE_RANK=$i
+    break
+  fi
+done
