a3284b4653181f406774a2ba2cb2af5e14977309
cy <chenyang08056032@163.com>
2026-03-27 15:34:27 +0800

    add

---
+++ Long-term-stability-tese/397B/Qwen3.5-397B-A17B-w4a8.sh
@@ -0,0 +1,49 @@
+# high performance cpu
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+sysctl -w vm.swappiness=0
+sysctl -w kernel.numa_balancing=0
+sysctl -w kernel.sched_migration_cost_ns=50000
+# bind cpu
+export SGLANG_SET_CPU_AFFINITY=1
+
+unset https_proxy
+unset http_proxy
+unset HTTPS_PROXY
+unset HTTP_PROXY
+export ASCEND_LAUNCH_BLOCKING=1
+# cann
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+source /usr/local/Ascend/nnal/atb/set_env.sh
+cd /home/hexq/mtp/sglang_project
+export PYTHONPATH=${PWD}/python:$PYTHONPATH
+export ASCEND_USE_FIA=1
+export STREAMS_PER_DEVICE=32
+export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=64
+export HCCL_BUFFSIZE=2500
+export HCCL_OP_EXPANSION_MODE=AIV
+export HCCL_SOCKET_IFNAME=lo
+export GLOO_SOCKET_IFNAME=lo
+export SGLANG_NPU_PROFILING=1
+export SGLANG_NPU_PROFILING_STAGE="prefill"
+export DEEPEP_NORMAL_LONG_SEQ_ROUND=32
+export DEEPEP_NORMAL_LONG_SEQ_PER_ROUND_TOKENS=4096
+export SGLANG_ENABLE_SPEC_V2=1
+export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=1
+
+python3 -m sglang.launch_server \
+        --model-path /root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-397B-A17B-w8a8-mtp \
+        --attention-backend ascend \
+        --device npu \
+        --tp-size 16 --nnodes 1 --node-rank 0 \
+        --chunked-prefill-size -1 --max-prefill-tokens 131072 \
+        --disable-radix-cache \
+        --trust-remote-code \
+        --host 127.0.0.1 --max-running-requests 256 \
+        --mem-fraction-static 0.85 \
+        --port 8000 \
+        --cuda-graph-bs 1 2 3 4 8 9 10 11 12 13 14 15 16 \
+        --quantization modelslim \
+        --enable-multimodal --moe-a2a-backend deepep --deepep-mode auto \
+        --mm-attention-backend ascend_attn --max-total-tokens 600000 \
+        --dtype bfloat16 --mamba-ssm-dtype bfloat16 --speculative-draft-model-quantization unquant \
+        --speculative-algorithm NEXTN --speculative-num-steps 3 --speculative-eagle-topk 1 --speculative-num-draft-tokens 4
