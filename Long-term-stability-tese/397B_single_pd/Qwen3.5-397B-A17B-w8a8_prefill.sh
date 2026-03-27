6cd98d1c8c60a374726d78a9dc434cd4bb717b7d
cy <chenyang08056032@163.com>
2026-03-27 17:55:34 +0800

    add

---
+++ Long-term-stability-tese/397B_single_pd/Qwen3.5-397B-A17B-w8a8_prefill.sh
@@ -0,0 +1,50 @@
+# high performance cpu
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+sysctl -w vm.swappiness=0
+sysctl -w kernel.numa_balancing=0
+sysctl -w kernel.sched_migration_cost_ns=50000
+# bind cpu
+export SGLANG_SET_CPU_AFFINITY=1
+
+unset https_proxy
+unset http_proxy
+unset HTTPS_PROXY
+unset HTTP_PROXY
+export ASCEND_LAUNCH_BLOCKING=1
+# cann
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+source /usr/local/Ascend/nnal/atb/set_env.sh
+#cd /home/hexq/mtp/sglang_project
+#export PYTHONPATH=${PWD}/python:$PYTHONPATH
+export SGLANG_DISAGGREGATION_WAITING_TIMEOUT=3600
+export STREAMS_PER_DEVICE=32
+export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=128
+export HCCL_BUFFSIZE=2500
+export HCCL_OP_EXPANSION_MODE=AIV
+export HCCL_SOCKET_IFNAME=lo
+export GLOO_SOCKET_IFNAME=lo
+export SGLANG_NPU_PROFILING=0
+export SGLANG_NPU_PROFILING_STAGE="prefill"
+export DEEPEP_NORMAL_LONG_SEQ_ROUND=32
+export DEEPEP_NORMAL_LONG_SEQ_PER_ROUND_TOKENS=3584
+export ASCEND_MF_STORE_URL="tcp://172.22.3.181:24669"
+export SGLANG_DISAGGREGATION_BOOTSTRAP_TIMEOUT=3600
+export SGLANG_DISAGGREGATION_WAITING_TIMEOUT=3600
+export SGLANG_ENABLE_SPEC_V2=1
+export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=1
+python3 -m sglang.launch_server \
+        --model-path /home/weights/Qwen3.5-397B-A17B-w8a8 \
+        --attention-backend ascend \
+        --device npu \
+        --tp-size 8 --nnodes 1 --node-rank 0 \
+        --chunked-prefill-size -1 --max-prefill-tokens 28672 \
+        --disable-radix-cache \
+        --trust-remote-code \
+        --host 172.22.3.181 --max-running-requests 1 \
+        --mem-fraction-static 0.85 \
+        --port 8999 \
+        --cuda-graph-bs 2 4 6 8 10 16 20 24 28 32 48 64 128 \
+        --quantization modelslim \
+        --enable-multimodal --skip-server-warmup  --disable-cuda-graph --disaggregation-mode prefill --disaggregation-transfer-backend ascend  \
+        --mm-attention-backend ascend_attn  --moe-a2a-backend deepep --deepep-mode normal --speculative-draft-model-quantization unquant \
+        --dtype bfloat16 --mamba-ssm-dtype bfloat16 --speculative-algorithm NEXTN --speculative-num-steps 3 --speculative-eagle-topk 1 --speculative-num-draft-tokens 4
