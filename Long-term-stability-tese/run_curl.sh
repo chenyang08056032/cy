3d7872f83bc817c6bd65fc0d765460d1b93bac68
cy <chenyang08056032@163.com>
2026-03-27 16:59:54 +0800

    add

--- Long-term-stability-tese/run_curl.sh
+++ Long-term-stability-tese/run_curl.sh
@@ -3,22 +3,30 @@ curl --location http://172.22.3.181:6689/v1/chat/completions --header 'Content-T
     {"type": "text", "text": "The capital of France is"}
   ]}], "temperature": 0, "chat_template_kwargs":{"enable_thinking":false}
   }'
-  curl --location http://172.22.3.181:6689/v1/chat/completions --header 'Content-Type: application/json' --data '{ "model": "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-397B-A17B-w8a8-mtp","max_tokens": 1000,"messages": [{ "role": "user",
+echo " "
+echo " "
+curl --location http://172.22.3.181:6689/v1/chat/completions --header 'Content-Type: application/json' --data '{ "model": "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-397B-A17B-w8a8-mtp","max_tokens": 1000,"messages": [{ "role": "user",
  "content": [
     {"type": "text", "text": "1+1=几"}
   ]}], "temperature": 0, "chat_template_kwargs":{"enable_thinking":false}
   }'
-  curl --location http://172.22.3.181:6689/v1/chat/completions --header 'Content-Type: application/json' --data '{ "model": "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-397B-A17B-w8a8-mtp","max_tokens": 1000,"messages": [{ "role": "user",
+echo " "
+echo " "
+curl --location http://172.22.3.181:6689/v1/chat/completions --header 'Content-Type: application/json' --data '{ "model": "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-397B-A17B-w8a8-mtp","max_tokens": 1000,"messages": [{ "role": "user",
  "content": [
     {"type": "text", "text": "20乘3等于多少"}
   ]}], "temperature": 0, "chat_template_kwargs":{"enable_thinking":false}
   }'
-  curl --location http://172.22.3.181:6689/v1/chat/completions --header 'Content-Type: application/json' --data '{ "model": "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-397B-A17B-w8a8-mtp","max_tokens": 1000,"messages": [{ "role": "user",
+echo " "
+echo " "
+curl --location http://172.22.3.181:6689/v1/chat/completions --header 'Content-Type: application/json' --data '{ "model": "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-397B-A17B-w8a8-mtp","max_tokens": 1000,"messages": [{ "role": "user",
  "content": [
     {"type": "text", "text": "9除以3等于多少"}
   ]}], "temperature": 0, "chat_template_kwargs":{"enable_thinking":false}
   }'
-  curl --location http://172.22.3.181:6689/v1/chat/completions --header 'Content-Type: application/json' --data '{ "model": "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-397B-A17B-w8a8-mtp","max_tokens": 1000,"messages": [{ "role": "user",
+echo " "
+echo " "
+curl --location http://172.22.3.181:6689/v1/chat/completions --header 'Content-Type: application/json' --data '{ "model": "/root/.cache/modelscope/hub/models/Eco-Tech/Qwen3.5-397B-A17B-w8a8-mtp","max_tokens": 1000,"messages": [{ "role": "user",
  "content": [
     {"type": "text", "text": "10+5+6等于多少"}
   ]}], "temperature": 0, "chat_template_kwargs":{"enable_thinking":false}
