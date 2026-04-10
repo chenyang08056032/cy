be966c4543b456e73e63984c99a107a965d66639
cy <chenyang08056032@163.com>
2026-04-10 20:21:54 +0800

    add

--- 111.pu.py
+++ 111.pu.py
@@ -5,7 +5,7 @@ import unittest
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import DOTS_OCR_WEIGHTS_PATH
+# from sglang.test.ascend.test_ascend_utils import DOTS_OCR_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase, popen_launch_server, DEFAULT_URL_FOR_TEST, \
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
