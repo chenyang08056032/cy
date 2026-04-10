c47ba3dbbc91da866ebb51a400e6ac02cd9a244b
cy <chenyang08056032@163.com>
2026-04-10 20:16:36 +0800

    add

--- 111.pu.py
+++ 111.pu.py
@@ -1,10 +1,11 @@
 import json
+import logging
 import unittest
 
 import requests
 
 from sglang.srt.utils import kill_process_tree
-# from sglang.test.ascend.test_ascend_utils import DOTS_OCR_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import DOTS_OCR_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase, popen_launch_server, DEFAULT_URL_FOR_TEST, \
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
@@ -15,9 +16,51 @@ register_npu_ci(
     nightly=True,
 )
 
+PROMPT_TEXT = """Please output the layout information from the PDF image, including each layout element's bbox, its category, and the corresponding text content within the bbox.
 
-class TestDeepseekVl2(CustomTestCase):
-    """Testcase: Verify that the inference accuracy of the rednote-hilab/dots.ocr model on the MMMU dataset is no less than 0.2.
+    1. Bbox format: [x1, y1, x2, y2]
+
+    2. Layout Categories: The possible categories are ['Caption', 'Footnote', 'Formula', 'List-item', 'Page-footer', 'Page-header', 'Picture', 'Section-header', 'Table', 'Text', 'Title'].
+
+    3. Text Extraction & Formatting Rules:
+        - Picture: For the 'Picture' category, the text field should be omitted.
+        - Formula: Format its text as LaTeX.
+        - Table: Format its text as HTML.
+        - All Others (Text, Title, etc.): Format their text as Markdown.
+
+    4. Constraints:
+        - The output text must be the original text from the image, with no translation.
+        - All layout elements must be sorted according to human reading order.
+
+    5. Final Output: The entire output must be a single JSON object.
+    """
+
+PAYLOAD = {
+    "model": "dots_ocr",
+    "messages": [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "/data/c30044170/dataset/images/invoice_with_barcode_logo.jpeg"
+                    }
+                },
+                {
+                    "type": "text",
+                    "text": PROMPT_TEXT
+                }
+            ]
+        }
+    ],
+    "max_tokens": 2000,
+    "temperature": 0
+}
+
+
+class TestDotsOcr(CustomTestCase):
+    """Testcase: Tests dots.ocr model OCR accuracy on invoice image.
 
     [Test Category] Model
     [Test Target] rednote-hilab/dots.ocr
@@ -25,8 +68,7 @@ class TestDeepseekVl2(CustomTestCase):
 
     @classmethod
     def setUpClass(cls):
-        # cls.model = DOTS_OCR_WEIGHTS_PATH
-        cls.model = "/root/.cache/modelscope/hub/models/rednote-hilab/dots.ocr"
+        cls.model = DOTS_OCR_WEIGHTS_PATH
         cls.base_url = DEFAULT_URL_FOR_TEST
         other_args = [
             "--max-prefill-tokens",
@@ -70,58 +112,19 @@ class TestDeepseekVl2(CustomTestCase):
         kill_process_tree(cls.process.pid)
 
     def test_npu_ocr(self):
-        # 配置变量
+        """Test OCR model accuracy on invoice image."""
         url = f"{self.base_url}/v1/chat/completions"
         headers = {"Content-Type": "application/json"}
-        prompt_text = """Please output the layout information from the PDF image, including each layout element's bbox, its category, and the corresponding text content within the bbox.
 
-        1. Bbox format: [x1, y1, x2, y2]
-
-        2. Layout Categories: The possible categories are ['Caption', 'Footnote', 'Formula', 'List-item', 'Page-footer', 'Page-header', 'Picture', 'Section-header', 'Table', 'Text', 'Title'].
-
-        3. Text Extraction & Formatting Rules:
-            - Picture: For the 'Picture' category, the text field should be omitted.
-            - Formula: Format its text as LaTeX.
-            - Table: Format its text as HTML.
-            - All Others (Text, Title, etc.): Format their text as Markdown.
-
-        4. Constraints:
-            - The output text must be the original text from the image, with no translation.
-            - All layout elements must be sorted according to human reading order.
-
-        5. Final Output: The entire output must be a single JSON object.
-        """
-
-        payload = {
-            "model": "dots_ocr",
-            "messages": [
-                {
-                    "role": "user",
-                    "content": [
-                        {
-                            "type": "image_url",
-                            "image_url": {
-                                "url": "/data/c30044170/dataset/images/invoice_with_barcode_logo.jpeg"
-                            }
-                        },
-                        {
-                            "type": "text",
-                            "text": prompt_text
-                        }
-                    ]
-                }
-            ],
-            "max_tokens": 2000,
-            "temperature": 0
-        }
-
-        response = requests.post(url, headers=headers, json=payload)
-        print(response.json())
+        response = requests.post(url, headers=headers, json=PAYLOAD)
+        logging.info(response.json())
         self.assertEqual(response.status_code, 200)
+
         ocr_result = json.loads(response.json()["choices"][0]["message"]["content"])
 
         detected_texts_set = {item["text"] for item in ocr_result
                               if item.get("category") in ["Text", "Section-header"] and "text" in item}
+        logging.info(detected_texts_set)
 
         expected_texts_set = {
             "8:36",
@@ -150,8 +153,7 @@ class TestDeepseekVl2(CustomTestCase):
             "Direct flights only.",
             "SEARCH FLIGHTS"
         }
-        print("")
-        print(detected_texts_set)
+
         self.assertEqual(detected_texts_set, expected_texts_set,
                          f"Missing texts: {expected_texts_set - detected_texts_set}")
 
